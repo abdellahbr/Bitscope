ecf3aa384bb45d98bd0d6df57700c7f389862ca4
Nicocco <nicolas.morisseau53@gmail.com>
2017-10-31 18:38:24 +0000

    Ajout de l'affichage et la configuration du matériel

--- Acquisition.py
+++ Acquisition.py
@@ -12,6 +12,7 @@ avec le bitscoepe. elle prend comme attribut de classe :
         - un nombre de points à capturer"""
 
     def __init__(self, NbPoints, Voie):
+        print "Recherche de votre matériel ... "
         self.MY_SIZE = NbPoints
 	self.DATA = [0]*self.MY_SIZE
         if Voie != "A" and Voie!= "B":
@@ -35,6 +36,7 @@ avec le bitscoepe. elle prend comme attribut de classe :
 	BL_Range(BL_Count(BL_COUNT_RANGE))
 	BL_Offset(BL_ZERO)
         BL_Enable(TRUE)
+        print "Matériel détecté ! (%s)" %BL_Name(0)
 
     def __del__(self):
         """ destructeur de la classe """
@@ -43,6 +45,7 @@ avec le bitscoepe. elle prend comme attribut de classe :
 
     def Infos(self):
         """ Information sur la session d'acquisition """
+        print "-------------- INFOS -------------\n"
         print "Port de connection : %s" %BL_Name(0)
         print "version du Bitscope : %s (%s)" %(BL_Version(BL_VERSION_DEVICE),BL_ID())
         print "Canales : %d (%d analogiques(s) + %d logique(s))" %(BL_Count(
@@ -55,10 +58,17 @@ avec le bitscoepe. elle prend comme attribut de classe :
         if BL_Offset(-1000) != BL_Offset(1000):
             print "  Offset: %+.4gV to %+.4gV" % (
                 BL_Offset(1000), BL_Offset(-1000))
-
+        print "\n--------------------------------"
 
     def Enregistrer(self):
         """ Méthode permettant d'enregistrer suivant les paramètre de l'instance
 en cour"""   
 	BL_Trace()
 	self.DATA = BL_Acquire()
+
+    def DisplayAcq(self):
+        """Renvoi les valeur aquise par l'instance de la classe en cours """
+        print "------------- DATA --------------\n\n"
+        print "\t" .join(["%f" % self.DATA[i] for i in range (len(self.DATA))])
+        print "\n--------------------------------"
+
---
+++ test.py
@@ -0,0 +1,7 @@
+from Acquisition import*
+
+Acq = Acquisition(300,"A")
+#Acq.Enregistrer()
+
+Acq.Infos()
+Acq.DisplayAcq()
