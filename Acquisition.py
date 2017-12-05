e94830a40733ed34003b6ff6bd39f2cc286ad812
Nicocco <nicolas.morisseau53@gmail.com>
2017-12-05 23:00:53 +0100

    ajout de la fonction help (non fini)

--- Acquisition.py
+++ Acquisition.py
@@ -192,29 +192,139 @@ en cour"""
 ###############################################################################
 
     """ Fonction d'aide permet d'avoir tout les infos sur le bitscope """
-    def Help(self,info = "g"):
+    def Help(self,info = "0"):
         for i in range(len(info)):
             self.sub_Help(str(info[i]))
             
             
     def sub_Help(self,Cara):
-        if Cara=="g":
-            message = "La fonction d'aide prend plusieurs paramètre qu'il convient de passer en paramètre. "
-            message = message + "Ces paramètres sont des lettres sensible à la casse. \n\n"
-            message = message + "\"d\"\t-->\t Extrait de la dataSheet du btscope\n\n"
-            message = message + "\"\""
-            print message
+        if Cara=="0":
+            message = "La fonction d'aide vous permet d'obtenir des
+informations sur l'utilisation de votre bitscope en ligne de commande. Il vous
+suffit de passer en paramètre de la fonction Help une chaine de caractère
+contenant l'un ou un ensemble concaténé des caratère suivant :\n\n"
+            message = message + "\"d\"\t-->\t Totalité de la dataSheet du btscope\n\n"
+            message = message + "\"i\"\t--> Documentation sur les Inputs\n\n"
+            message = message + "\"a\"\t--> Documentation sur les Acquisitions\n\n"
+            message = message + "\"t\"\t--> Documentation sur les triggers\n\n"
+            message = message + "\"g\"\t--> Documentation général sur le
+bitscope\n\n"
+            message = message + "Pour obtenir de l'aide sur une méthode taper
+son nom en paramètre de la fonction help. LA liste des méthodes est données si
+dessous :\n\n"
+            message = message + "SetMode, setVoie, setDuration, Enregistrer,
+DisplayAcq, DisplayTime, Infos"
+             print message
 
         if Cara =="d":
-            message = "Valeur critique extraite de la datasheet à ne pas dépasser :\n\n"
-            message = message + "Fréquence d'échantillonnage max : 20 MHz\n"
-            message = message + "Tension d'entrée max : 5 V\n"
-            message = message + "Mémoire tampon : 12 Ko\n"
-            message = message + "Impédence des entrées analogique : 1 MOhm (+- 1%) , 1pF\n"
-            message = message + "Impédance des entrées logiques : 100 KOhm (+- 1%), 10pF\n"
-            message = message + "Niveau des entrées logiques : 3.3 / 5 V CMOS (compatible TTL)\n"
-            message = message + "Sensibilité maximal : 5 mV (<1 MHz)\n"
-            message = message + "Sensibilité entré analogique : 20 mV (bande complète)\n"
-            message = message + "Fréquence d'affichage : 50 Hz(20 ms)\n"
-            message = message + "\n"
+            self.sub_Help("g")
+            self.sub_Help("i")
+            self.sub_Help("a")
+            self.sub_Help("t")
+
+        if Cara =="i":
+            message = "---------- INPUTS ----------\n\n"
+            message = message + "Analog Bandwith : 20 MHz\n"
+            message = message + "Capture channels : 2 analog + 8 logic\n"
+            message = message + "Input Ranges : 1.1 V ~ 11 V\n"
+            message = message + "Vertical Scalling : 20 mV/Div ~ 2 V/div\n"
+            message = message + "Vertical Acuracy : +- 4% (full scale)\n"
+            message = message + "Analog sensitivity : 20 mV (full bandwith)\n"
+            message = message + "Maximum sensitivity : 5 mV(<1 MHz)\n"
+            message = message + "Protocole capture : UART, SPI and I2C\n"
+            message = message + "Input Offsets : Yes (manual only)\n"
+            message = message + "Input Sensing : Yes\n"
+            message = message + "Adjustable Switching : Yes (D6 and D7)\n"
+            message = message + "Analog Input Impedence : 1 MOhms +- 1%, 10 pF\n"
+            message = message + "Logic Input Impedence : 100 KOhms +- 1%, 10 pF
+(logic)\n"
+            message = message + "Logic Input levels : 3.3/5 V CMOS (TTL
+Compatible)\n"
+            print message
+
+        if Cara == "a":
+            message = "---------- ACQUISITION ----------\n\n"
+            message = message + "Real-Time Mixed Signal : Yes\n"
+            message = message + "Mixed Signal Streaming : Yes\n"
+            message = message + "Macro High Resolution : Yes\n"
+            message = message + "Sub-Sampled Analog : No\n"
+            message = message + "Protocol Streaming : No\n"
+            message = message + "Digital Sample Rate (Max) : 40 MSps (per
+frame)\n"
+            message = message + "Analog Sample Rate (Max) : 20 MSps (per
+frame)\n"
+            message = message + "Streaming Rate (Max) : 200 KSps (continous)\n"
+            message = message + "Native Resolution : 8/12 bits(switchable)\n"
+            message = message + "12 ENOB (<1 MHz)\n"
+            message = message + "Display Frame Rate : 50Hz (20 ms)\n"
+            message = message + "Capture Buffers : 12 KS, 6 KS x 2, 6 KS x 9 or
+3 KS x 2 + 6 KS x 8\n"
+            message = message + "Timerbase Range : 1us/Div ~ 100 ms/Div\n"
+            message = message + "Timebase Accuracy : 0.01% (100 ppm)\n"
+            print message
+
+        if Cara =="t":
+            message = "---------- TRIGGERS ----------\n\n"
+            message = message + "Trigger Modes : Edge (Rise/Fall), Level/State
+& Logic\n"
+            message = message + "Hysteresis / Sensitivity : +- 2%\n"
+            message = message + "Trigger Filter : Fast, Normal & Delay\n"
+            message = message + "Cross-Trigger Ops : Logic trigger analog &
+vice versa\n"
+            message = message + "trigger Delay Timebase : 100 us to 10 s
+(programmable)\n"
+            message = message + "Trigger Hold-Off : 1 ms ~ 100 ms\n"
+            print message
+
+        if Cara == "g":
+            message = "---------- GENERAL ----------\n\n"
+            message = message + "Power requierement : 5 V USB powered\n"
+            message = message + "Operating Temperature : -40°C ~ +40°C\n"
+            message = message + "Water Resistant : Yes\n"
+            message = message + "Dimensions (WxDxH) : 20 x 110 x 8 mm\n"
+            message = message + "Weight : 12 g\n"
+            print message
+
+        if Cara == "setMode":
+            message = "Permet la selection du mode d'aqcuisition de la sonde
+:\n\n"
+            message = message + "FAST --> Acquisition rapide d'une seul voie\n"
+            message = message + "DUAL --> Acquisition des deux voie
+simultanément\n"
+            message = message + "MIXED --> Acquisition de sur voie analogique
+et logique simultanément\n"
+            message = message + "LOGIC --> Acquisition sur les voie logiques\n"
+            message = message + "STREAM --> \n"
+            message = message + "\nExemple d'utilisation :
+Acquisition.setMode(\"FAST\")"
+            print message
+
+        if Cara == "setDuration":
+            message = "Permet de réglé la durée d'acquisition en seconde. le changement du temps d'acquisition induit un changement
+automatique du nombre de point (conservation de la fréquence d'échantillonnage)\n"
+            print message
+
+        if Cara == "setVoie":
+            message = "Permet de choisir la voie d'acquisition si l'aqcuisition n'est pas une acquisition sur voie
+logique.\n"
+            message = message + "\nExemple d'utilisation : Acquisition.setVoie(\"A\")"
+            print message
+
+        if Cara == "Enregistrer":
+            message = "Permet de lancer l'acquisition préalablement paramétré.\n"
+            print message
+
+        if Cara == "DisplayAcq":
+            message = "Permet d'afficher le tableau des valeurs acquise dans la console. Si l'acquisition n'a pas été
+lancée avec Enregistrer, le tableau est initialement rempli de 0\n"
+            print message
+
+        if Cara == "DisplayTime":
+            message = "Permet d'afficher le tableau des temps d'acquisition dans la console. Si l'acquisition n'a pas
+été lancée avec Enregistrer, le tableau est initialement rempli de 0\n"
+            print message
+
+        if Cara == "Infos":
+            message = "Permet d'afficher les informations de configuration de votre Bitscope et de l'acquisition en
+cours\n"
             print message
