33e2433fdef2be5b6e294c9bae348868a1fa11d3
Nicocco <nicolas.morisseau53@gmail.com>
2017-12-06 12:22:14 +0100

    Arangement fichier

--- setup.sh
+++ setup.sh
@@ -8,8 +8,8 @@ then
   # mettre installation de la bitlib
   echo "Archi 64 bit détecté"
   fi
-  sudo cp TR_Acquisition.py /usr/lib/python2.7 
-  sudo cp Acquisition.py  /usr/lib/python2.7
+  sudo cp Modules/TR_Acquisition.py /usr/lib/python2.7 
+  sudo cp Modules/Acquisition.py  /usr/lib/python2.7
 else
   echo "La version 2.7 de python est requise, elle ne semble pas être installé sur votre distribution linux"
 fi
